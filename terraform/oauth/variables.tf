9f1fe1e69a04e9072fc7770dd883f5986de23ad0
Neffi42 <me@neffi.fr>
2026-07-28 22:28:01 +0200

    chore: add default value to oauth/var.kanidm_url

--- terraform/oauth/variables.tf
+++ terraform/oauth/variables.tf
@@ -1,6 +1,7 @@
 variable "kanidm_url" {
   description = "Url of the kanidm instance to administrate"
   type        = string
+  default     = "https://auth.neffi.fr/"
 }
 
 variable "kanidm_token" {
